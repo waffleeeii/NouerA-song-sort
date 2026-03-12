48df64e30fa395c3a6985e88e804383ad28b7cc5
waffleeeii <hibara_ai1023@naver.com>
2026-03-13 06:34:17 +0900

    update songs

--- src/songs.js
+++ src/songs.js
@@ -1,20 +1,20 @@
 //songs to sort
-/*export const songs = [
-   "Disease",
-   "Abracadabra",
-   "Garden of Eden",
-   "Perfect Celebrity",
-   "Vanish Into You",
-   "Killah",
-   "ZombieBoy",
-   "LoveDrug",
-   "How Bad Do U Want Me",
-   "Don't Call Tonight",
-   "Shadow of a Man",
-   "The Beast",
-   "Blade of Grass",
-   "Die With a Smile",
-   "Can't Stop the High (Bonus Track)",
-   "Kill for Love (Bonus Track)",
+export const songs = [
+   { name:"POP IT LIKE" },
+   { name:"Silhouette" },
+   { name:"A-LIST" },
+   { name:"We Are Young" },
+   { name:"N.I.N(New is Now)" },
+   { name:"Sparkle" },
+   { name:"무한대(mu)" },
+   { name:"Pairing" },
+   { name:"Jet Stream" },
+   { name:"Bring the Crunk" },
+   { name:"BNB(Beauty and the Beast" },
+   { name:"FINALE" },
+   { name:"WARNING!" },
+   { name:"MAKE YOU MOVE" },
+   { name:"n(number of cases)" },
+   { name:"MYSELF" },
   ];
-  export default songs;*/
+  export default songs;
